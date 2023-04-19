3c656b96398d3b13b30df824dbd46835fde0e18f
Salem Akowe <salemakowe@gmail.com>
2023-04-19 06:53:20 +0100

    .

--- riverpod_1/lib/homepage.dart
+++ riverpod_1/lib/homepage.dart
@@ -1,3 +1,45 @@
+/* Stateless widget (ConsumerWidget) for Riverpod
+
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter/material.dart';
+
+final mycounter = StateProvider((ref) => 0);
+
+class HomePage extends ConsumerWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("RiverpodTest"),
+        centerTitle: true,
+        actions: [],
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            Consumer(
+              builder: (context, ref, _) {
+                final count = ref
+                    .watch(mycounter); //watching the StateProvider for change
+                return Text('$count');
+              },
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => ref.read(mycounter.notifier).state++,
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
+
+*/
+
+//Stateful widget (ConsumerStateful) widget for Riverpod
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
